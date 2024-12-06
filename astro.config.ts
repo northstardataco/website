a384f916a7b244f7cd18c15e2d995dad5eea29ba
Emil Sadek <esadek@users.noreply.github.com>
2024-12-06 11:51:24 -0800

    Change: formatting

--- astro.config.ts
+++ astro.config.ts
@@ -1,8 +1,8 @@
-import { defineConfig } from 'astro/config';
-import react from '@astrojs/react';
-import tailwind from '@astrojs/tailwind';
+import { defineConfig } from "astro/config";
+import react from "@astrojs/react";
+import tailwind from "@astrojs/tailwind";
 
 export default defineConfig({
   site: "https://northstardata.co",
-  integrations: [react(), tailwind()]
+  integrations: [react(), tailwind()],
 });
