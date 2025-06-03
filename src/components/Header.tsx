dfaac941edb2ff08485bc672075e1edc851accf1
Emil Sadek <esadek@users.noreply.github.com>
2025-06-03 15:39:58 +0200

    Fix: responsive header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,7 +2,8 @@ export default function Header() {
   return (
     <p className="text-4xl">
       Data engineering and analytics
-      <br />
+      <br className="hidden sm:block" />
+      <span className="sm:hidden">&nbsp;</span>
       <span className="text-slate-500">for a simple monthly fee</span>
     </p>
   );
